b03bb4ee2ecb64aa4ba9647afd122e15e12e652d
astrizhkin <astrizhkin@biarum.com>
2025-02-12 21:51:33 +0200

    fix outdated erase

--- range_sensor_layer/src/range_sensor_layer.cpp
+++ range_sensor_layer/src/range_sensor_layer.cpp
@@ -388,14 +388,15 @@ void RangeSensorLayer::removeOutdatedReadings()
   std::map<std::pair<unsigned int, unsigned int>, double>::iterator it_map;
 
   double removal_time = last_reading_time_.toSec() - pixel_decay_;
-  for (it_map = marked_point_history_.begin() ; it_map != marked_point_history_.end() ; it_map++ )
+  for (it_map = marked_point_history_.begin() ; it_map != marked_point_history_.end() ; )
   {
-    if(it_map->second < removal_time)
-    {
+    if(it_map->second < removal_time) {
       removed++;
       ROS_INFO_STREAM("-");
-      marked_point_history_.erase(it_map);
       setCost(std::get<0>(it_map->first), std::get<1>(it_map->first), costmap_2d::FREE_SPACE);
+      it_map = marked_point_history_.erase(it_map);
+    } else {
+      it_map++;
     }
   }
   ROS_INFO_STREAM("[range_sensor_layer] End remove outdated " << removed);
@@ -431,25 +432,19 @@ void RangeSensorLayer::update_cell(
     unsigned char c = to_cost(new_prob);
 
     setCost(x, y, c);
-    if(use_decay_)
-    {
+    if(use_decay_) {
       std::pair<unsigned int, unsigned int> coordinate_pair(x, y);
       // If the point has a score high enough to be marked in the costmap, we add it's time to the marked_point_history
       if(c > to_cost(mark_threshold_)) {
-        //ROS_INFO_STREAM("+");
         marked_point_history_[coordinate_pair] = last_reading_time_.toSec();
-        //ROS_INFO_STREAM("*");
-      }
+      } 
       // If the point score is not high enough, we try to find it in the mark history point.
       // In the case we find it in the marked_point_history we clear it from the map so we won't checked already cleared point
-      else if(c < to_cost(clear_threshold_))
-      {
-        //ROS_INFO_STREAM("-");
+      else if(c < to_cost(clear_threshold_)) {
         std::map<std::pair<unsigned int, unsigned int>, double>::iterator it_clear;
         it_clear = marked_point_history_.find(coordinate_pair);
         if(it_clear != marked_point_history_.end()) {
           marked_point_history_.erase(it_clear);
-          //ROS_INFO_STREAM("*");
         }
       }
     }
